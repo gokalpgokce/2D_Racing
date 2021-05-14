31ca0a8753dec0dd99b5436516b04cef6405197a
gokalpgokce <gokalpgokce7@gmail.com>
2021-05-14 15:06:02 +0300

    Add Mute Button

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     public AudioSource carSound;
     public AudioSource maxSpeedSound;
+    public Toggle isMute;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,16 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       soundToggle(isMute.isOn);
     }
+
+    public void soundToggle (bool isMute){
+        if(isMute == true){
+            AudioListener.volume = 1;
+        }
+        else if (isMute == false){
+            AudioListener.volume = 0;
+        }
+    }
+
 }
